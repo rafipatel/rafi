858d3ab25269b1f46952edc13c285bb8e2b8e62a
Rafiahmed Patel <rafipatel3562@gmail.com>
2025-10-26 12:01:26 +0000

    changes

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,6 @@ import react from "@vitejs/plugin-react-swc";
 import path from "path";
 import { componentTagger } from "lovable-tagger";
 
-// https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
   base: "/rafi/", // ← add this line
   server: {
